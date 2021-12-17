1e81a50bcaa7a3c7311adc8172efcf8dd1742671
jihodaddy <94466572+jihodaddy@users.noreply.github.com>
2021-12-17 23:14:38 +0900

    Add files via upload

---
+++ React/my-app/src/components/Image.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Image extends Component {
+    render() {
+        return (
+            <div>
+                <img src="../image/apple.png"></img>
+            </div>
+        );
+    }
+}
+
+export default Image;
